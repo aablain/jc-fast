2ba6ff0a01d2a958b5ee7a52c2e9d4135ec3c9b0
Austin Blain <austin.a.blain@gmail.com>
2017-05-22 03:54:09 -0700

    add styling for day pages and pseudo youtube API work

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Days } from '../days.model';
 import { Day } from '../day.model';
+declare var $:any;
 
 @Component({
   selector: 'app-homepage',
@@ -24,6 +25,37 @@ export class HomepageComponent implements OnInit {
   goToDayDetailsPage(clickedDay: Day) {
     this.router.navigate(["day" + clickedDay.dayid]);
     console.log(clickedDay.dayid);
+  }
+
+  showFirstVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/jSF6oE8Y3fk");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(1)").addClass("currentVid");
+  }
+  showSecondVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/IgDHQ93H7KI");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(2)").addClass("currentVid");
+  }
+  showThirdVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/Rh7uGdyLMmQ");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(3)").addClass("currentVid");
+  }
+  showFourthVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/mJ_LN9BC2Ng");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(4)").addClass("currentVid");
+  }
+  showFifthVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/yTSH3euFKVE");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(5)").addClass("currentVid");
+  }
+  showSixthVideo(item) {
+    $("#player").attr("src","https://www.youtube.com/embed/tI6GRY20Tp4?list=PLM5wn1MwyZJkkPTvmvjlM2Jj1CLUUxfRg");
+    $(".currentVid").removeClass("currentVid");
+    $(".homepage__youtube__videosBox__video:nth-of-type(6)").addClass("currentVid");
   }
 
 }
